36e9b153c1e616db7c6d1ddc5c18e750b838c0ed
duslerke <43747286+Duslerke@users.noreply.github.com>
2022-06-29 20:52:14 +0100

    Added the script file that will host a bulk of update commands to the core script.

---
+++ updateScript.sh
@@ -0,0 +1,9 @@
+# set -e # Stop executing if any of the commands fail
+
+echo "Updating..."
+
+# Drop in a list of update commands here in the following form:
+./action-updateNonHaloPRNs.sh "tenure_id_1" "new_PRN_1"
+./action-updateNonHaloPRNs.sh "tenure_id_2" "new_PRN_2"
+
+echo -e "\nUpdate done!"
